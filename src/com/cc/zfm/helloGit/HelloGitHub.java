383d2254e2d53424870dd4669a58204db1bed0ef
zhFanmao <zh_fanmao@163.com>
2017-10-30 22:23:39 +0800

    3333

--- src/com/cc/zfm/helloGit/HelloGitHub.java
+++ src/com/cc/zfm/helloGit/HelloGitHub.java
@@ -3,6 +3,7 @@ package com.cc.zfm.helloGit;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello git....") ;
-		System.out.println("hello git too....") ;
+		System.out.println("hello git too....");
+		System.out.println("hello git 3....");
 	}
 }
