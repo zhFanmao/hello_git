747a61064dc4d6cd013f796b5820f97bb4da0ebf
zhFanmao <zh_fanmao@163.com>
2017-10-30 22:11:47 +0800

    too

--- src/com/cc/zfm/helloGit/HelloGitHub.java
+++ src/com/cc/zfm/helloGit/HelloGitHub.java
@@ -2,6 +2,7 @@ package com.cc.zfm.helloGit;
 
 public class HelloGitHub {
 	public static void main(String[] args) {
-		System.out.println("hello git....");
+		System.out.println("hello git....") ;
+		System.out.println("hello git too....") ;
 	}
 }
